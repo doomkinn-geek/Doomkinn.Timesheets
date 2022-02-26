2267e52163e4bab0a64b7f65624daec1d8f3fa4a
doomkinn@yandex.ru <doomkinn@yandex.ru>
2022-02-26 15:45:16 +0300

    ...

--- WebApplication3/Data/DB/DataDBContext.cs
+++ WebApplication3/Data/DB/DataDBContext.cs
@@ -1,8 +1,19 @@
-﻿using System.Data.Entity;
+﻿using Doomkinn.Timesheets.Models;
+using System.Data.Entity;
 
 namespace Doomkinn.Timesheets.Data.DB
 {
     public class DataDBContext : DbContext
-    {        
+    {
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DataDBContext(string connectionString):base(connectionString)  
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
     }
 }
