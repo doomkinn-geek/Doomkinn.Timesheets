6db67b5737d26f0c62dc0c0ac456dd9f06286859
doomkinn@yandex.ru <doomkinn@yandex.ru>
2022-03-18 22:00:09 +0300

    updated from lesson 7

---
+++ WebApplication3/Models/Invoice.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doomkinn.Timesheets.Models
+{
+    internal class Invoice
+    {
+        public int Id { get; set; }
+        public DateTime DateStart { get; private set; }
+        public DateTime DateEnd { get; private set; }
+        public int Sum { get; private set; }
+
+        public List<Sheet> Sheets { get; }
+
+        public Invoice()
+        {
+            Sheets = new List<Sheet>();
+        }
+
+        public void IncludeSheets(Sheet sheet)
+        {
+            Sheets.Add(sheet);
+        }
+        public void Initialize (DateTime dateStart, int sum)
+        {
+            this.DateStart = dateStart;
+            this.Sum = sum;
+        }
+    }
+}
---
+++ WebApplication3/Models/Sheet.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doomkinn.Timesheets.Models
+{
+    internal class Sheet
+    {
+		public int Id { get; private set; }
+		public DateTime ApproveDate { get; private set; }
+		public bool IsApproved { get; private set; }
+		public int Amount { get; set; }
+
+		public void Approve()
+		{
+			IsApproved = true;
+			ApproveDate = DateTime.Now;
+		}
+
+	}
+}
